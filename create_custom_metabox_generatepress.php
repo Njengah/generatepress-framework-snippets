86bf19a6bfae91fb49eda3b805e85de470355a92
Njengah <joe@njengah.com>
2020-04-24 23:49:09 +0300

    generate custom metabox

---
+++ create_custom_metabox_generatepress.php
@@ -0,0 +1,25 @@
+<?php 
+
+add_action( 'add_meta_boxes', 'generate_register_custom_meta_box' );
+/**
+ * Generate a custom metabox
+ */
+function generate_register_custom_meta_box() {
+	if ( ! current_user_can( apply_filters( 'generate_metabox_capability', 'edit_theme_options' ) ) ) {
+		return;
+	}
+
+	$post_types = get_post_types( array( 'public' => true ) );
+
+	foreach ( $post_types as $type ) {
+		if ( 'post' !== $type ) {
+			add_meta_box(
+				'generate_custom_meta_box',
+				esc_html__( 'Custom Metabox', 'generatepress' ),
+				'generate_do_layout_meta_box',
+				$type,
+				'side'
+			);
+		}
+	}
+}
